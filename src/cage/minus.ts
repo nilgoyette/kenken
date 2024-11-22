132e094b0d696cb61d2685e4593aeaeba182b153
Nil Goyette <nil.goyette@proton.me>
2024-11-22 15:04:13 -0500

    Init for minus -

---
+++ src/cage/minus.ts
@@ -0,0 +1,17 @@
+import { Cage } from "./lib.ts";
+import { range } from "../cell.ts";
+
+export class Minus extends Cage {
+    init(): void {
+        const N = this.n + 1;
+        if (this.result >= N / 2) {
+            const stop =  this.n - this.result;
+            const x1 = range(1, stop + 1);
+            const x2 = range(N - stop, N);
+            const set = new Set(x1.concat(x2));
+            this.set_to_all_cells(set);
+        } else {
+            this.set_to_all_cells(new Set(range(1, N)));
+        }
+    }
+}
