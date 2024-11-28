a10430800724b7e256a0cc16cb06228df4a566ed
Nil Goyette <nil.goyette@proton.me>
2024-11-28 14:35:27 -0500

    Cage Minus respects its base class

--- src/cage/minus.ts
+++ src/cage/minus.ts
@@ -3,13 +3,6 @@ import { range } from "../cell.ts";
 
 export class Minus extends Cage {
     init(): void  {
-        const all = range(1, this.n + 1);
-        for (const cell of this.cells) {
-            cell.possibilities = new Set(all);
-        }
-    }
-
-    solve(): Removal[] {
         const N = this.n + 1;
         if (this.result >= N / 2) {
             const stop =  this.n - this.result;
@@ -20,6 +13,31 @@ export class Minus extends Cage {
         } else {
             this.set_to_all_cells(new Set(range(1, N)));
         }
+    }
+
+    solve(): Removal[] {
+        const c1 = this.cells[0];
+        const c2 = this.cells[1];
+        c1.possibilities = solve_side(c1.possibilities, c2.possibilities, this.result);
+        c2.possibilities = solve_side(c2.possibilities, c1.possibilities, this.result);
         return [];
     }
+}
+
+function solve_side(p1: Set<number>, p2: Set<number>, result: number): Set<number> {
+    const new_possibilities = new Set<number>;
+    for (const n1 of p1) {
+        let at_least_one = false;
+        for (const n2 of p2) {
+            const d = Math.abs(n1 - n2);
+            if (d == result) {
+                at_least_one = true;
+                break;
+            }
+        }
+        if (at_least_one) {
+            new_possibilities.add(n1);
+        }
+    }
+    return new_possibilities;
 }
